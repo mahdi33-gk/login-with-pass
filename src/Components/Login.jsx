504451bb9e751e355a80e22f368d6b5f6e798f7d
mehedi <mehedi25122003@gmail.com>
2025-04-10 10:39:26 +0600

    firebase init file edited

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,18 +1,30 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
+import auth from "../firebase.init";
 
 const Login = () => {
-  const [err, setErr] = useState('');
-  const summitHandler = (e) =>{
+  const [err, setErr] = useState("");
+  const summitHandler = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.pass.value;
 
-    const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
-    if(!strongPasswordRegex.test(password)){
-      setErr('Please add one letter one number and one special charachter.')
+    const strongPasswordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/;
+    if (!strongPasswordRegex.test(password)) {
+      setErr("Please add one letter one number and one special charachter.");
       return;
     }
-  }
+
+    // createEmailUser
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        setErr("somethings not right.try again later.");
+      });
+  };
   return (
     <div>
       <div className="hero  min-h-screen">
@@ -20,26 +32,35 @@ const Login = () => {
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Login now!</h1>
             <p className="py-6">
-              Practice for building real life project..Firebase just start now...
+              Practice for building real life project..Firebase just start
+              now...
             </p>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="fieldset-label">Email</label>
-                <input name="email" type="email" className="input" placeholder="Email" />
-                <label className="fieldset-label">Password</label>
-                <input
-                  name="pass"
-                  type="password"
-                  className="input"
-                  placeholder="Password"
-                />
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
-                </div>
-                <button className="btn btn-neutral mt-4">Login</button>
-              </fieldset>
+              <form onSubmit={summitHandler}>
+                <fieldset className="fieldset">
+                  <label className="fieldset-label">Email</label>
+                  <input
+                    name="email"
+                    type="email"
+                    className="input"
+                    placeholder="Email"
+                  />
+                  <label className="fieldset-label">Password</label>
+                  <input
+                    name="pass"
+                    type="password"
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <div>
+                    <a className="link link-hover">Forgot password?</a>
+                  </div>
+                  <button className="btn btn-neutral mt-4">Login</button>
+                  {err && <p>{err}</p>}
+                </fieldset>
+              </form>
             </div>
           </div>
         </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,6 +1,7 @@
+
+import { getAuth } from "firebase/auth";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkT13Lsv7feObv5iIAKow6muoFw__4mtc",
-  authDomain: "loginemailpass-f5392.firebaseapp.com",
-  projectId: "loginemailpass-f5392",
-  storageBucket: "loginemailpass-f5392.firebasestorage.app",
-  messagingSenderId: "323223098876",
-  appId: "1:323223098876:web:e62c4fc567cf68a4300e79",
-  measurementId: "G-2B9EX1SVW4"
+  apiKey: "AIzaSyAPgdE_hGueHtXukVYHcVNdMQdQxzWWKew",
+  authDomain: "loginpagelast.firebaseapp.com",
+  projectId: "loginpagelast",
+  storageBucket: "loginpagelast.firebasestorage.app",
+  messagingSenderId: "470669517023",
+  appId: "1:470669517023:web:03ae53588b47c023e2896a",
+  measurementId: "G-WZELHL9FRT"
 };
 
 // Initialize Firebase
